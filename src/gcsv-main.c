5c8f4fc950165184ea4c5e61250adc26acb1f71c
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2015-08-04 15:16:32 +0200

    Run a GtkApplication with an empty window

---
+++ src/gcsv-main.c
@@ -0,0 +1,54 @@
+/*
+ * This file is part of gCSVedit.
+ *
+ * Copyright 2015 - Université Catholique de Louvain
+ *
+ * gCSVedit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gCSVedit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gCSVedit.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
+ */
+
+#include <gtk/gtk.h>
+#include "gcsv-window.h"
+
+static void
+activate_cb (GtkApplication *app)
+{
+	GcsvWindow *window;
+
+	window = gcsv_window_new ();
+	gtk_application_add_window (app, GTK_WINDOW (window));
+	gtk_widget_show (GTK_WIDGET (window));
+}
+
+gint
+main (gint    argc,
+      gchar **argv)
+{
+	GtkApplication *app;
+	gint status;
+
+	app = gtk_application_new ("org.ucl.gcsvedit", G_APPLICATION_FLAGS_NONE);
+
+	g_signal_connect (app,
+			  "activate",
+			  G_CALLBACK (activate_cb),
+			  NULL);
+
+	status = g_application_run (G_APPLICATION (app), argc, argv);
+
+	g_object_unref (app);
+
+	return status;
+}
---
+++ src/gcsv-window.c
@@ -0,0 +1,63 @@
+/*
+ * This file is part of gCSVedit.
+ *
+ * Copyright 2015 - Université Catholique de Louvain
+ *
+ * gCSVedit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gCSVedit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gCSVedit.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
+ */
+
+#include "gcsv-window.h"
+
+struct _GcsvWindow
+{
+	GtkApplicationWindow parent;
+};
+
+G_DEFINE_TYPE (GcsvWindow, gcsv_window, GTK_TYPE_APPLICATION_WINDOW)
+
+static void
+gcsv_window_dispose (GObject *object)
+{
+
+	G_OBJECT_CLASS (gcsv_window_parent_class)->dispose (object);
+}
+
+static void
+gcsv_window_finalize (GObject *object)
+{
+
+	G_OBJECT_CLASS (gcsv_window_parent_class)->finalize (object);
+}
+
+static void
+gcsv_window_class_init (GcsvWindowClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+	object_class->dispose = gcsv_window_dispose;
+	object_class->finalize = gcsv_window_finalize;
+}
+
+static void
+gcsv_window_init (GcsvWindow *window)
+{
+}
+
+GcsvWindow *
+gcsv_window_new (void)
+{
+	return g_object_new (GCSV_TYPE_WINDOW, NULL);
+}
