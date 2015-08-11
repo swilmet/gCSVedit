2489c2ce86f893b75311e55a9eadd03c17f2b29c
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2015-08-11 12:12:59 +0200

    Simple unit tests for the alignment

---
+++ tests/test-alignment.c
@@ -0,0 +1,104 @@
+/*
+ * This file is part of gCSVedit.
+ *
+ * Copyright 2015 - Université Catholique de Louvain
+ *
+ * gCSVedit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gCSVedit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gCSVedit.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
+ */
+
+#include "gcsv-alignment.h"
+#include <gtk/gtk.h>
+
+static gchar *
+get_buffer_text (GtkTextBuffer *buffer)
+{
+	GtkTextIter start;
+	GtkTextIter end;
+
+	gtk_text_buffer_get_bounds (buffer, &start, &end);
+	return gtk_text_buffer_get_text (buffer, &start, &end, TRUE);
+}
+
+static void
+check_alignment (const gchar *before,
+		 const gchar *after,
+		 gunichar     delimiter)
+{
+	GtkTextBuffer *buffer;
+	GcsvAlignment *align;
+	gchar *buffer_text;
+
+	buffer = gtk_text_buffer_new (NULL);
+	gtk_text_buffer_set_text (buffer, before, -1);
+
+	align = gcsv_alignment_new (buffer, delimiter);
+	buffer_text = get_buffer_text (buffer);
+	g_assert_cmpstr (buffer_text, ==, after);
+	g_free (buffer_text);
+
+	/* Test unalignment */
+	g_object_unref (align);
+	buffer_text = get_buffer_text (buffer);
+	g_assert_cmpstr (buffer_text, ==, before);
+	g_free (buffer_text);
+
+	/* Test alignment update */
+	gtk_text_buffer_set_text (buffer, "", -1);
+	align = gcsv_alignment_new (buffer, delimiter);
+	gtk_text_buffer_set_text (buffer, before, -1);
+	gcsv_alignment_update (align);
+	buffer_text = get_buffer_text (buffer);
+	g_assert_cmpstr (buffer_text, ==, after);
+	g_free (buffer_text);
+
+	g_object_unref (align);
+	g_object_unref (buffer);
+}
+
+static void
+test_commas (void)
+{
+	check_alignment ("aaa,bbb\n"
+			 "1,2\n"
+			 "10,20",
+			 /**/
+			 "aaa,bbb\n"
+			 "1  ,2  \n"
+			 "10 ,20 ",
+			 /**/
+			 ',');
+
+	check_alignment ("a,b,c,\n"
+			 "1,2\n"
+			 "xx,yy,Zzz",
+			 /**/
+			 "a ,b ,c  ,\n"
+			 "1 ,2 \n"
+			 "xx,yy,Zzz",
+			 /**/
+			 ',');
+}
+
+gint
+main (gint    argc,
+      gchar **argv)
+{
+	gtk_test_init (&argc, &argv);
+
+	g_test_add_func ("/align/commas", test_commas);
+
+	return 0;
+}
