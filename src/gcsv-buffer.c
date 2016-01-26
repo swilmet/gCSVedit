b1b711ce1ea89aff64905e59859ee7f21a06c1a2
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2016-01-26 11:53:36 +0100

    Add 2016 copyright in recently modified files

--- src/gcsv-buffer.c
+++ src/gcsv-buffer.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of gCSVedit.
  *
- * Copyright 2015 - Université Catholique de Louvain
+ * Copyright 2015, 2016 - Université Catholique de Louvain
  *
  * gCSVedit is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
