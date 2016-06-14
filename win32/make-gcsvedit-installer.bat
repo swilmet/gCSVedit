3dd0133a1039abd7d352a45f58341a70e8e62283
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2016-06-14 14:29:42 +0200

    win32: fix second path to bash.exe in *.bat script
    C:/msys64bis/tmp/newgcsvedit/ doesn't exist after stage1.

--- win32/make-gcsvedit-installer.bat
+++ win32/make-gcsvedit-installer.bat
@@ -3,7 +3,7 @@ if errorlevel 1 (
 exit /b %errorlevel%
 )
 
-C:/msys64bis/tmp/newgcsvedit/msys64/usr/bin/bash.exe -c "./make-gcsvedit-installer stage2"
+C:/msys64bis/usr/bin/bash.exe -c "./make-gcsvedit-installer stage2"
 if errorlevel 1 (
 exit /b %errorlevel%
 )
