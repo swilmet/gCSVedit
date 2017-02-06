c2df03647a0acf7006215055c2581b06cd0d530d
Sébastien Wilmet <swilmet@gnome.org>
2017-02-06 11:11:16 +0100

    Create GcsvApplication class
    It's a good practice to have a GtkApplication subclass instead of doing
    everything in the main().

---
+++ src/gcsv-application.c
@@ -0,0 +1,328 @@
+/*
+ * This file is part of gCSVedit.
+ *
+ * Copyright 2017 - Sébastien Wilmet <swilmet@gnome.org>
+ *
+ * gCSVedit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gCSVedit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gCSVedit.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+#include "gcsv-application.h"
+#include <gtef/gtef.h>
+#include <glib/gi18n.h>
+#include "gcsv-window.h"
+
+#ifdef G_OS_WIN32
+#  include <io.h>
+#  include <conio.h>
+#  ifndef _WIN32_WINNT
+#    define _WIN32_WINNT 0x0501
+#  endif
+#  include <windows.h>
+#endif
+
+struct _GcsvApplicationPrivate
+{
+	gint something;
+};
+
+static gboolean option_version;
+
+static GOptionEntry options[] = {
+	{ "version", 'v',
+	  G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, &option_version,
+	  N_("Display the version and exit"),
+	  NULL
+	},
+
+        { NULL }
+};
+
+G_DEFINE_TYPE_WITH_PRIVATE (GcsvApplication, gcsv_application, GTK_TYPE_APPLICATION)
+
+static GcsvWindow *
+get_active_gcsv_window (GcsvApplication *app)
+{
+	GList *windows;
+	GList *l;
+
+	windows = gtk_application_get_windows (GTK_APPLICATION (app));
+
+	for (l = windows; l != NULL; l = l->next)
+	{
+		GtkWindow *window = l->data;
+
+		if (GCSV_IS_WINDOW (window))
+		{
+			return GCSV_WINDOW (window);
+		}
+	}
+
+	return NULL;
+}
+
+static gint
+gcsv_application_handle_local_options (GApplication *app,
+				       GVariantDict *options_dict)
+{
+	if (option_version)
+	{
+		g_print ("%s %s\n", g_get_application_name (), PACKAGE_VERSION);
+		return 0;
+	}
+
+	if (G_APPLICATION_CLASS (gcsv_application_parent_class)->handle_local_options != NULL)
+	{
+		return G_APPLICATION_CLASS (gcsv_application_parent_class)->handle_local_options (app, options_dict);
+	}
+
+	return -1;
+}
+
+static void
+quit_activate_cb (GSimpleAction *quit_action,
+		  GVariant      *parameter,
+		  gpointer       user_data)
+{
+	GcsvApplication *app = GCSV_APPLICATION (user_data);
+
+	while (TRUE)
+	{
+		GcsvWindow *window = get_active_gcsv_window (app);
+
+		if (window != NULL && gcsv_window_close (window))
+		{
+			continue;
+		}
+
+		break;
+	}
+}
+
+static void
+about_activate_cb (GSimpleAction *about_action,
+		   GVariant      *parameter,
+		   gpointer       user_data)
+{
+	GcsvApplication *app = GCSV_APPLICATION (user_data);
+	GcsvWindow *active_window = get_active_gcsv_window (app);
+
+	const gchar *authors[] = {
+		"Sébastien Wilmet <sebastien.wilmet@uclouvain.be>",
+		NULL
+	};
+
+	const gchar *copyright =
+		"Copyright 2015-2016 – Université Catholique de Louvain\n"
+		"Copyright 2015-2016 – Sébastien Wilmet";
+
+	gtk_show_about_dialog (GTK_WINDOW (active_window),
+			       "name", g_get_application_name (),
+			       "version", PACKAGE_VERSION,
+			       "comments", _("gCSVedit is a small and lightweight CSV text editor"),
+			       "authors", authors,
+			       "translator-credits", _("translator-credits"),
+			       "website", PACKAGE_URL,
+			       "website-label", _("gCSVedit website"),
+			       "logo-icon-name", "accessories-text-editor",
+			       "license-type", GTK_LICENSE_GPL_3_0,
+			       "copyright", copyright,
+			       NULL);
+}
+
+/* Code taken from gedit. */
+#ifdef G_OS_WIN32
+static void
+setup_path (void)
+{
+	gchar *installdir;
+	gchar *bin;
+	gchar *path;
+
+	installdir = g_win32_get_package_installation_directory_of_module (NULL);
+
+	bin = g_build_filename (installdir, "bin", NULL);
+	g_free (installdir);
+
+	/* Set PATH to include the gedit executable's folder */
+	path = g_build_path (";", bin, g_getenv ("PATH"), NULL);
+	g_free (bin);
+
+	if (!g_setenv ("PATH", path, TRUE))
+	{
+		g_warning ("Could not set PATH for gCSVedit.");
+	}
+
+	g_free (path);
+}
+
+static void
+prep_console (void)
+{
+	/* If we open the application from a console get the stdout printing */
+	if (fileno (stdout) != -1 &&
+	    _get_osfhandle (fileno (stdout)) != -1)
+	{
+		/* stdout is fine, presumably redirected to a file or pipe */
+	}
+	else
+	{
+		typedef BOOL (* WINAPI AttachConsole_t) (DWORD);
+
+		AttachConsole_t p_AttachConsole =
+			(AttachConsole_t) GetProcAddress (GetModuleHandle ("kernel32.dll"),
+							  "AttachConsole");
+
+		if (p_AttachConsole != NULL && p_AttachConsole (ATTACH_PARENT_PROCESS))
+		{
+			freopen ("CONOUT$", "w", stdout);
+			dup2 (fileno (stdout), 1);
+			freopen ("CONOUT$", "w", stderr);
+			dup2 (fileno (stderr), 2);
+		}
+	}
+}
+#endif /* G_OS_WIN32 */
+
+static void
+gcsv_application_startup (GApplication *app)
+{
+	gchar *metadata_filename;
+
+	/* GActions */
+	const GActionEntry app_entries[] = {
+		{ "quit", quit_activate_cb },
+		{ "about", about_activate_cb },
+	};
+
+	if (G_APPLICATION_CLASS (gcsv_application_parent_class)->startup != NULL)
+	{
+		G_APPLICATION_CLASS (gcsv_application_parent_class)->startup (app);
+	}
+
+	g_action_map_add_action_entries (G_ACTION_MAP (app),
+					 app_entries, G_N_ELEMENTS (app_entries),
+					 app);
+
+	/* Init metadata manager */
+	metadata_filename = g_build_filename (g_get_user_cache_dir (),
+					      "gcsvedit",
+					      "gcsvedit-metadata.xml",
+					      NULL);
+
+	gtef_metadata_manager_init (metadata_filename);
+
+	g_free (metadata_filename);
+
+#ifdef G_OS_WIN32
+	setup_path ();
+	prep_console ();
+#endif
+}
+
+static void
+gcsv_application_activate (GApplication *app)
+{
+	GcsvWindow *window;
+
+	if (G_APPLICATION_CLASS (gcsv_application_parent_class)->activate != NULL)
+	{
+		G_APPLICATION_CLASS (gcsv_application_parent_class)->activate (app);
+	}
+
+	window = gcsv_window_new (GTK_APPLICATION (app));
+	gtk_widget_show (GTK_WIDGET (window));
+}
+
+static void
+gcsv_application_open (GApplication  *app,
+		       GFile        **files,
+		       gint           n_files,
+		       const gchar   *hint)
+{
+	GcsvWindow *active_window;
+	GcsvWindow *window;
+
+	if (n_files < 1)
+	{
+		return;
+	}
+
+	if (n_files > 1)
+	{
+		g_warning ("Opening several files at once is not supported.");
+	}
+
+	active_window = get_active_gcsv_window (GCSV_APPLICATION (app));
+
+	if (active_window != NULL && gcsv_window_is_untouched (active_window))
+	{
+		window = active_window;
+	}
+	else
+	{
+		window = gcsv_window_new (GTK_APPLICATION (app));
+		gtk_widget_show (GTK_WIDGET (window));
+	}
+
+	gcsv_window_load_file (window, files[0]);
+}
+
+static void
+gcsv_application_shutdown (GApplication *app)
+{
+	/* Chain-up first, so we are sure that everything in GTK+ is shutdown
+	 * and we can shutdown our stuff. If we chain-up after our code, GTK+
+	 * can still hold some references to some objects, and those objects can
+	 * still save settings, metadata, etc in their destructors.
+	 */
+	if (G_APPLICATION_CLASS (gcsv_application_parent_class)->shutdown != NULL)
+	{
+		G_APPLICATION_CLASS (gcsv_application_parent_class)->shutdown (app);
+	}
+
+	gtef_metadata_manager_shutdown ();
+}
+
+static void
+gcsv_application_class_init (GcsvApplicationClass *klass)
+{
+	GApplicationClass *gapp_class = G_APPLICATION_CLASS (klass);
+
+	gapp_class->handle_local_options = gcsv_application_handle_local_options;
+	gapp_class->startup = gcsv_application_startup;
+	gapp_class->activate = gcsv_application_activate;
+	gapp_class->open = gcsv_application_open;
+	gapp_class->shutdown = gcsv_application_shutdown;
+}
+
+static void
+gcsv_application_init (GcsvApplication *app)
+{
+	app->priv = gcsv_application_get_instance_private (app);
+
+	g_set_application_name ("gCSVedit");
+	gtk_window_set_default_icon_name ("accessories-text-editor");
+
+	g_application_add_main_option_entries (G_APPLICATION (app), options);
+}
+
+GcsvApplication *
+gcsv_application_new (void)
+{
+	return g_object_new (GCSV_TYPE_APPLICATION,
+			     "application-id", "be.uclouvain.gcsvedit",
+			     "flags", G_APPLICATION_HANDLES_OPEN,
+			     NULL);
+}
---
+++ src/gcsv-application.h
@@ -0,0 +1,56 @@
+/*
+ * This file is part of gCSVedit.
+ *
+ * Copyright 2017 - Sébastien Wilmet <swilmet@gnome.org>
+ *
+ * gCSVedit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gCSVedit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gCSVedit.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GCSV_APPLICATION_H
+#define GCSV_APPLICATION_H
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define GCSV_TYPE_APPLICATION             (gcsv_application_get_type ())
+#define GCSV_APPLICATION(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), GCSV_TYPE_APPLICATION, GcsvApplication))
+#define GCSV_APPLICATION_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), GCSV_TYPE_APPLICATION, GcsvApplicationClass))
+#define GCSV_IS_APPLICATION(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GCSV_TYPE_APPLICATION))
+#define GCSV_IS_APPLICATION_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), GCSV_TYPE_APPLICATION))
+#define GCSV_APPLICATION_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), GCSV_TYPE_APPLICATION, GcsvApplicationClass))
+
+typedef struct _GcsvApplication         GcsvApplication;
+typedef struct _GcsvApplicationClass    GcsvApplicationClass;
+typedef struct _GcsvApplicationPrivate  GcsvApplicationPrivate;
+
+struct _GcsvApplication
+{
+	GtkApplication parent;
+
+	GcsvApplicationPrivate *priv;
+};
+
+struct _GcsvApplicationClass
+{
+	GtkApplicationClass parent_class;
+};
+
+GType			gcsv_application_get_type	(void);
+
+GcsvApplication *	gcsv_application_new		(void);
+
+G_END_DECLS
+
+#endif /* GCSV_APPLICATION_H */
