56b91b5a47d7f24eaca183efa44ad1b1e489846b
Sébastien Wilmet <swilmet@gnome.org>
2019-12-26 13:33:19 +0100

    win32: update make-gcsvedit-installer.bat
    IIRC the "bis" was because I had two installations of msys2, the first
    one was not working and I was probably unable to delete it.

--- build-aux/win32/make-gcsvedit-installer.bat
+++ build-aux/win32/make-gcsvedit-installer.bat
@@ -1,9 +1,9 @@
-C:/msys64bis/usr/bin/bash.exe -c "./make-gcsvedit-installer stage1"
+C:/msys64/usr/bin/bash.exe -c "./make-gcsvedit-installer stage1"
 if errorlevel 1 (
 exit /b %errorlevel%
 )
 
-C:/msys64bis/usr/bin/bash.exe -c "./make-gcsvedit-installer stage2"
+C:/msys64/usr/bin/bash.exe -c "./make-gcsvedit-installer stage2"
 if errorlevel 1 (
 exit /b %errorlevel%
 )
