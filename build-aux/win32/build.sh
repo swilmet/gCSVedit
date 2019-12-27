84ba2742c3e271224693ffc133959f454a0f9104
Sébastien Wilmet <sebastien.wilmet@gmail.com>
2019-12-27 13:06:59 +0100

    win32: build.sh: install_gcsvedit_packages()

--- build-aux/win32/build.sh
+++ build-aux/win32/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+_version='0.7.0'
 _pacman_root=/tmp/gcsvedit
+_mingw_package_dir=mingw-w64-gcsvedit
 
 create_pacman_root() {
 	rm -rf "${_pacman_root}"
@@ -16,4 +18,15 @@ create_pacman_root() {
 	pacman -S filesystem bash pacman --noconfirm --root "${_pacman_root}" || exit 1
 }
 
+install_gcsvedit_packages() {
+	pushd "${_mingw_package_dir}" > /dev/null
+
+	./build.sh
+	pacman --upgrade mingw-w64-x86_64-gcsvedit-${_version}-1-any.pkg.tar.xz \
+		--noconfirm --root "${_pacman_root}" || exit 1
+
+	popd > /dev/null
+}
+
 create_pacman_root
+install_gcsvedit_packages
