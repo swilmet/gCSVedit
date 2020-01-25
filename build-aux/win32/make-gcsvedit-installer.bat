34cc21102fd72e297397a3efc1b6c7203c166002
Sébastien Wilmet <swilmet@gnome.org>
2020-01-25 11:05:53 +0100

    win32: remove old installer
    make-gcsvedit-installer has already been replaced partially by the
    build.sh script.
    
    The old installer created a *.msi file. We want a *.msix file now.

--- build-aux/win32/make-gcsvedit-installer.bat
+++
@@ -1,9 +0,0 @@
-C:/msys64/usr/bin/bash.exe -c "./make-gcsvedit-installer stage1"
-if errorlevel 1 (
-exit /b %errorlevel%
-)
-
-C:/msys64/usr/bin/bash.exe -c "./make-gcsvedit-installer stage2"
-if errorlevel 1 (
-exit /b %errorlevel%
-)
