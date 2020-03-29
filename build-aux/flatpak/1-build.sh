b77d25c12aa9ebcf0418f70c511ebb11e7da66a1
Sébastien Wilmet <swilmet@gnome.org>
2020-03-29 21:53:46 +0200

    flatpak: convert the manifest to Yaml

--- build-aux/flatpak/1-build.sh
+++ build-aux/flatpak/1-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm -rf gcsvedit/ repo/
-flatpak-builder gcsvedit com.github.swilmet.gcsvedit.json || exit 1
+flatpak-builder gcsvedit com.github.swilmet.gcsvedit.yml || exit 1
 flatpak build-export repo gcsvedit
